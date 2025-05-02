59a8c16025d76515ca3c35211f81024b519cda02
aleksandarcolic22414 <acacolic@gmail.com>
2025-05-02 17:16:02 +0200

    Extracted pointer tagging to separate file.

--- art.h
+++ art.h
@@ -15,6 +15,8 @@
 #include <string_view>
 #include <vector>
 
+#include "ptr_tag.h"
+
 // NOLINTBEGIN
 
 namespace art {
@@ -104,34 +106,6 @@ private:
     const uint8_t* const m_data;
     size_t m_size;
 };
-
-// Pointer tagging.
-// Since malloc's returned address is guaranteed to be aligned at least as std::max_align_t, we will
-// use unused last bits in pointer to store additonal info. This can be used to simulate base class
-// and virtual method calls.
-//
-static constexpr uintptr_t tag_bits = alignof(std::max_align_t) - 1;
-
-constexpr uintptr_t raw(const void* ptr) noexcept
-{
-    return std::bit_cast<uintptr_t>(ptr);
-}
-
-constexpr uintptr_t tag(const void* ptr) noexcept
-{
-    return raw(ptr) & tag_bits;
-}
-
-constexpr void* clear_tag(const void* ptr) noexcept
-{
-    return std::bit_cast<void*>(raw(ptr) & ~tag_bits);
-}
-
-constexpr void* set_tag(const void* ptr, uintptr_t tag) noexcept
-{
-    assert((tag & ~tag_bits) == 0);
-    return std::bit_cast<void*>(raw(clear_tag(ptr)) | (tag & tag_bits));
-}
 
 // Wrapper class that holds either pointer to node or pointer to leaf.
 // It is not a smart pointer, hence user must manage memory manually.
